b4fd894efb67e4112b6f096f116acbed6158b896
kewii33 <kewiii071@gmail.com>
2024-03-15 15:23:28 +0900

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const page = async () => {
-  const response = await fetch(`http://localhost:3000/api/company`);
+  const response = await fetch(`http://localhost:4000/companyInfo`);
   const companyInfo = await response.json();
   return (
     <div className="max-w-[1200px] min-w-[800px] h-[800px] mx-auto pt-[60px]">
       <div className="text-[#444] text-[30px] font-bold border-b-[1px] border-gray-500 pb-[10px]">
-        {companyInfo.companyInfo.name}
+        {companyInfo.name}
       </div>
-      <div className="pt-[30px] mb-[80px]">
-        {companyInfo.companyInfo.description}
-      </div>
+      <div className="pt-[30px] mb-[80px]">{companyInfo.description}</div>
       <div className="flex items-center flex justify-center mt-[30px]">
         <div className="w-[320px] h-[320px] flex flex-col flex justify-center flex items-center bg-gray-200 rounded-[12px]">
           <img
@@ -19,9 +17,9 @@ const page = async () => {
             className="w-[200px] h-[200px] mb-[20px]"
           />
           <div className="font-bold text-[20px] pb-[10px]">
-            {companyInfo.companyInfo.image.image1}
+            {companyInfo.image.image1}
           </div>
-          <div>{companyInfo.companyInfo.image.imageDescription1}</div>
+          <div>{companyInfo.image.imageDescription1}</div>
         </div>
         <div className="w-[320px] h-[320px] flex flex-col flex justify-center ml-[60px] flex items-center bg-gray-200 rounded-[12px]">
           <img
@@ -30,9 +28,9 @@ const page = async () => {
             className="w-[170px] h-[170px] mt-[15px]"
           />
           <div className="pt-[40px] font-bold text-[20px] pb-[10px]">
-            {companyInfo.companyInfo.image.image2}
+            {companyInfo.image.image2}
           </div>
-          <div>{companyInfo.companyInfo.image.imageDescription2}</div>
+          <div>{companyInfo.image.imageDescription2}</div>
         </div>
         <div className="w-[320px] h-[320px] flex flex-col flex justify-center ml-[80px] flex items-center bg-gray-200 rounded-[12px]">
           <img
@@ -41,9 +39,9 @@ const page = async () => {
             className="w-[200px] h-[200px] mb-[20px]"
           />
           <div className="font-bold text-[20px] pb-[10px]">
-            {companyInfo.companyInfo.image.image3}
+            {companyInfo.image.image3}
           </div>
-          <div>{companyInfo.companyInfo.image.imageDescription3}</div>
+          <div>{companyInfo.image.imageDescription3}</div>
         </div>
       </div>
     </div>
